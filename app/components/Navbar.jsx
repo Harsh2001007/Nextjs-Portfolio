e7875956394d8eada80b765192558f5c158d50b7
Harsh <hs5686584@gmail.com>
2025-05-14 00:18:40 +0530

    navbar part done

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,22 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 
 const Navbar = () => {
+  const sideMenuRef = useRef();
+
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(0rem)";
+  };
+
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(16rem)";
+  };
   return (
     <>
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+        <Image src={assets.header_bg_color} alt="" className="w-full" />
+      </div>
       <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex  items-center justify-between z-50">
         <a href="#top">
           <Image
@@ -13,34 +25,107 @@ const Navbar = () => {
             alt=""
           />
         </a>
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 shadow-sm bg-white bg-opacity-50">
           <li>
             <a href="#top" style={{ fontFamily: "ovo, sans-serif" }}>
               Home
             </a>
           </li>
           <li>
-            <a href="#about">About Me</a>
+            <a href="#about" style={{ fontFamily: "ovo, sans-serif" }}>
+              About Me
+            </a>
           </li>
           <li>
-            <a href="#services">Services</a>
+            <a href="#services" style={{ fontFamily: "ovo, sans-serif" }}>
+              Services
+            </a>
           </li>
           <li>
-            <a href="#work">My Work</a>
+            <a href="#work" style={{ fontFamily: "ovo, sans-serif" }}>
+              My Work
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact Me</a>
+            <a href="#contact" style={{ fontFamily: "ovo, sans-serif" }}>
+              Contact Me
+            </a>
           </li>
         </ul>
 
-        <div>
+        <div className="flex items-center gap-4">
+          <button className="flex items-center gap-4">
+            <Image src={assets.moon_icon} className="w-6" alt="" />
+          </button>
           <a
             href=""
             className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-black-500 rounded-full ml-4"
           >
             Contact <Image src={assets.arrow_icon} className="w-3 " alt="" />
           </a>
+          <button className="block md:hidden ml-3" onClick={openMenu}>
+            <Image src={assets.menu_black} className="w-6" alt="" />
+          </button>
         </div>
+
+        {/*----------- Mobile Menu --------------------  */}
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-0 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          </div>
+          <li>
+            <a
+              href="#top"
+              style={{ fontFamily: "ovo, sans-serif" }}
+              onClick={closeMenu}
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#about"
+              style={{ fontFamily: "ovo, sans-serif" }}
+              onClick={closeMenu}
+            >
+              About Me
+            </a>
+          </li>
+          <li>
+            <a
+              href="#services"
+              style={{ fontFamily: "ovo, sans-serif" }}
+              onClick={closeMenu}
+            >
+              Services
+            </a>
+          </li>
+          <li>
+            <a
+              href="#work"
+              style={{ fontFamily: "ovo, sans-serif" }}
+              onClick={closeMenu}
+            >
+              My Work
+            </a>
+          </li>
+          <li>
+            <a
+              href="#contact"
+              style={{ fontFamily: "ovo, sans-serif" }}
+              onClick={closeMenu}
+            >
+              Contact Me
+            </a>
+          </li>
+        </ul>
       </nav>
     </>
   );
