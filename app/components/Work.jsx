80e79eb0c52a33544aaa4e46e5c190dff8245024
Harsh <hs5686584@gmail.com>
2025-05-16 01:37:46 +0530

    service done && work page heading done

---
+++ app/components/Work.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Work = () => {
+  return (
+    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg" style={{ fontFamily: "ovo" }}>
+        My Portfolio
+      </h4>
+      <h2 className="text-center text-5xl" style={{ fontFamily: "ovo" }}>
+        My Latest Work
+      </h2>
+
+      <p
+        className="text-center max-w-2xl mx-auto mt-5 mb-12"
+        style={{ fontFamily: "ovo" }}
+      >
+        Welcome to my web development portfolio! Explore a collection of
+        projects showcasing my expertise in front-end development.
+      </p>
+    </div>
+  );
+};
+
+export default Work;
